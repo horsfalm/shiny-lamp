0731d5daeaf8fa5620a1a4265b5a5a478a9ec552
BTV <horsfalm@gmail.com>
2022-03-30 11:53:45 +1100

    deploying

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dontenv").config();
 
 const PORT = process.env.PORT || 3001;
 mongoose.connect(process.env.MONGODB_URI) || "mongodb://localhost/budget";
